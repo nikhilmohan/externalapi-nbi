9215999d6c2ecc609c1e0c29307ffcb6d92c0201
rakeshgirijaramesannair <rakeshgirijaramesannair@192.168.1.8>
2020-08-10 13:33:25 +0530

    TMF Post API SDC Client changes

--- src/main/java/org/onap/nbi/apis/servicecatalog/SdcClient.java
+++ src/main/java/org/onap/nbi/apis/servicecatalog/SdcClient.java
@@ -22,11 +22,15 @@ import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+
 import javax.annotation.PostConstruct;
+
 import org.apache.commons.io.IOUtils;
 import org.onap.nbi.OnapComponentsUrlPaths;
 import org.onap.nbi.exceptions.BackendFunctionalException;
@@ -42,6 +46,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -66,6 +71,8 @@ public class SdcClient {
 
     private static final String HEADER_ECOMP_INSTANCE_ID = "x-ecomp-instanceid";
     private static final String HEADER_AUTHORIZATION = "Authorization";
+    //changes for Post Implementation
+    private static final String USER_ID="USER_ID";
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SdcClient.class);
 
@@ -201,7 +208,69 @@ public class SdcClient {
             return null;
         }
     }
+    
+//changes for Post implementation start
+	/**
+	 * 
+	 * @param callURI
+	 * @param obj
+	 * @param userId
+	 * @return
+	 */
+	private ResponseEntity<Object> callSdcForPost(URI callURI, Object obj, String userId) {
+		try {
+			ResponseEntity<Object> response = restTemplate.exchange(callURI, HttpMethod.POST,
+					new HttpEntity<>(obj, buildRequestHeaderForPost(userId)), Object.class);
 
+			if (null == response) {
+				return null;
+			} else {
+
+				if (LOGGER.isDebugEnabled()) {
+					LOGGER.debug("response body : {} ", response.getBody().toString());
+				}
+				LOGGER.info("response status : {}", response.getStatusCodeValue());
+				return response;
+			}
+
+		} catch (BackendFunctionalException | ResourceAccessException e) {
+			LOGGER.error("Error on calling SDC API " + " ," + e.getMessage());
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	/**
+	 * 
+	 * @param serviceCatalogObject
+	 * @param userId
+	 */
+	public Map callPost(HashMap<Object, Object> serviceCatalogObject, String userId) {
+		// post url is the same as find url
+		UriComponentsBuilder callURI = UriComponentsBuilder.fromHttpUrl(sdcFindUrl);
+		ResponseEntity<Object> response = callSdcForPost(callURI.build().encode().toUri(), serviceCatalogObject,
+				userId);
+		// return (List<LinkedHashMap>) response.getBody();
+		return (LinkedHashMap) response.getBody();
+	}
+
+	/**
+	 * 
+	 * @param userId
+	 * @return
+	 */
+	private HttpHeaders buildRequestHeaderForPost(String userId) {
+		HttpHeaders httpHeaders = new HttpHeaders();
+
+		httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+		httpHeaders.add(HEADER_ECOMP_INSTANCE_ID, ecompInstanceId);
+		httpHeaders.add(HEADER_AUTHORIZATION, sdcHeaderAuthorization);
+		httpHeaders.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		httpHeaders.add(USER_ID, userId);
+
+		return httpHeaders;
+	}
+
+//changes for post implementation end
     private void loggDebugIfResponseKo(String callURI, ResponseEntity<Object> response) {
         if (LOGGER.isWarnEnabled() && !response.getStatusCode().equals(HttpStatus.OK)) {
             LOGGER.warn("HTTP call SDC on {} returns {} , {}", callURI, response.getStatusCodeValue(),
