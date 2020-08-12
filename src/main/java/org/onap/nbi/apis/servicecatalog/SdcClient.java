49ef1d21f40dafefe8ea22d3cc1593826ac7258b
rakeshgirijaramesannair <rakeshgirijaramesannair@192.168.1.8>
2020-08-12 11:55:37 +0530

    TMF API Post Changes

--- src/main/java/org/onap/nbi/apis/servicecatalog/SdcClient.java
+++ src/main/java/org/onap/nbi/apis/servicecatalog/SdcClient.java
@@ -22,11 +22,15 @@ import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+
 import javax.annotation.PostConstruct;
+
 import org.apache.commons.io.IOUtils;
 import org.onap.nbi.OnapComponentsUrlPaths;
 import org.onap.nbi.exceptions.BackendFunctionalException;
@@ -42,6 +46,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -52,160 +57,224 @@ import org.springframework.web.util.UriComponentsBuilder;
 @Service
 public class SdcClient {
 
-    @Autowired
-    private RestTemplate restTemplate;
+	@Autowired
+	private RestTemplate restTemplate;
 
-    @Value("${sdc.host}")
-    private String sdcHost;
+	@Value("${sdc.host}")
+	private String sdcHost;
 
-    @Value("${sdc.header.ecompInstanceId}")
-    private String ecompInstanceId;
+	@Value("${sdc.header.ecompInstanceId}")
+	private String ecompInstanceId;
 
-    @Value("${sdc.header.authorization}")
-    private String sdcHeaderAuthorization;
+	@Value("${sdc.header.authorization}")
+	private String sdcHeaderAuthorization;
 
-    private static final String HEADER_ECOMP_INSTANCE_ID = "x-ecomp-instanceid";
-    private static final String HEADER_AUTHORIZATION = "Authorization";
+	private static final String HEADER_ECOMP_INSTANCE_ID = "x-ecomp-instanceid";
+	private static final String HEADER_AUTHORIZATION = "Authorization";
+	// changes for Post Implementation
+	private static final String USER_ID = "USER_ID";
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(SdcClient.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(SdcClient.class);
 
-    private String sdcGetUrl;
-    private String sdcFindUrl;
-    private String sdcHealthCheck;
+	private String sdcGetUrl;
+	private String sdcFindUrl;
+	private String sdcHealthCheck;
 
-    @PostConstruct
-    private void setUpAndLogSDCUrl() {
-        sdcGetUrl = new StringBuilder().append(sdcHost)
-                .append(OnapComponentsUrlPaths.SDC_ROOT_URL + "/{id}" + OnapComponentsUrlPaths.SDC_GET_PATH).toString();
-        sdcFindUrl = new StringBuilder().append(sdcHost).append(OnapComponentsUrlPaths.SDC_ROOT_URL).toString();
-        sdcHealthCheck = new StringBuilder().append(sdcHost).append(OnapComponentsUrlPaths.SDC_HEALTH_CHECK).toString();
+	@PostConstruct
+	private void setUpAndLogSDCUrl() {
+		sdcGetUrl = new StringBuilder().append(sdcHost)
+				.append(OnapComponentsUrlPaths.SDC_ROOT_URL + "/{id}" + OnapComponentsUrlPaths.SDC_GET_PATH).toString();
+		sdcFindUrl = new StringBuilder().append(sdcHost).append(OnapComponentsUrlPaths.SDC_ROOT_URL).toString();
+		sdcHealthCheck = new StringBuilder().append(sdcHost).append(OnapComponentsUrlPaths.SDC_HEALTH_CHECK).toString();
 
-        LOGGER.info("SDC GET url :  " + sdcGetUrl);
-        LOGGER.info("SDC FIND url :  " + sdcFindUrl);
-        LOGGER.info("SDC HealthCheck :  " + sdcHealthCheck);
+		LOGGER.info("SDC GET url :  " + sdcGetUrl);
+		LOGGER.info("SDC FIND url :  " + sdcFindUrl);
+		LOGGER.info("SDC HealthCheck :  " + sdcHealthCheck);
 
-    }
+	}
 
-    public Map callGet(String id) {
+	public Map callGet(String id) {
 
-        String callUrl = sdcGetUrl.replace("{id}", id);
-        UriComponentsBuilder callURLFormated = UriComponentsBuilder.fromHttpUrl(callUrl);
-        ResponseEntity<Object> response = callSdc(callURLFormated.build().encode().toUri());
-        return (LinkedHashMap) response.getBody();
+		String callUrl = sdcGetUrl.replace("{id}", id);
+		UriComponentsBuilder callURLFormated = UriComponentsBuilder.fromHttpUrl(callUrl);
+		ResponseEntity<Object> response = callSdc(callURLFormated.build().encode().toUri());
+		return (LinkedHashMap) response.getBody();
 
-    }
+	}
 
-    public List<LinkedHashMap> callFind(MultiValueMap<String, String> parametersMap) {
+	public List<LinkedHashMap> callFind(MultiValueMap<String, String> parametersMap) {
 
-        UriComponentsBuilder callURI = UriComponentsBuilder.fromHttpUrl(sdcFindUrl);
-        if (parametersMap != null) {
-            Map<String, String> stringStringMap = parametersMap.toSingleValueMap();
-            for (Entry<String, String> entry : stringStringMap.entrySet()) {
-                if (!entry.getKey().equals("fields")) {
-                    callURI.queryParam(entry.getKey(), entry.getValue());
-                }
-            }
-        }
+		UriComponentsBuilder callURI = UriComponentsBuilder.fromHttpUrl(sdcFindUrl);
+		if (parametersMap != null) {
+			Map<String, String> stringStringMap = parametersMap.toSingleValueMap();
+			for (Entry<String, String> entry : stringStringMap.entrySet()) {
+				if (!entry.getKey().equals("fields")) {
+					callURI.queryParam(entry.getKey(), entry.getValue());
+				}
+			}
+		}
 
-        ResponseEntity<Object> response = callSdc(callURI.build().encode().toUri());
-        return (List<LinkedHashMap>) response.getBody();
+		ResponseEntity<Object> response = callSdc(callURI.build().encode().toUri());
+		return (List<LinkedHashMap>) response.getBody();
 
-    }
+	}
 
-    public LinkedHashMap callCheckConnectivity() {
+	public LinkedHashMap callCheckConnectivity() {
 
-        UriComponentsBuilder callURI = UriComponentsBuilder.fromHttpUrl(sdcHealthCheck);
-        ResponseEntity<Object> response = callSdc(callURI.build().encode().toUri());
-        return (LinkedHashMap) response.getBody();
+		UriComponentsBuilder callURI = UriComponentsBuilder.fromHttpUrl(sdcHealthCheck);
+		ResponseEntity<Object> response = callSdc(callURI.build().encode().toUri());
+		return (LinkedHashMap) response.getBody();
 
-    }
+	}
 
-    public File callGetWithAttachment(String toscaModelUrl) {
-        StringBuilder urlBuilder = new StringBuilder().append(sdcHost).append(toscaModelUrl);
+	public File callGetWithAttachment(String toscaModelUrl) {
+		StringBuilder urlBuilder = new StringBuilder().append(sdcHost).append(toscaModelUrl);
 
-        UriComponentsBuilder callURI = UriComponentsBuilder.fromHttpUrl(urlBuilder.toString());
+		UriComponentsBuilder callURI = UriComponentsBuilder.fromHttpUrl(urlBuilder.toString());
 
-        File directory = new File("temptoscafile");
-        if (!directory.exists()) {
-            directory.mkdir();
-        }
+		File directory = new File("temptoscafile");
+		if (!directory.exists()) {
+			directory.mkdir();
+		}
 
-        String fileName = "temptoscafile/" + System.currentTimeMillis() + "tosca.csar";
-        ResponseEntity<byte[]> response = callSdcWithAttachment(callURI.build().encode().toUri());
-        File toscaFile = new File(fileName);
-        try {
-            FileOutputStream toscaFileStream = new FileOutputStream(toscaFile);
-            if (response != null) {
-                IOUtils.write(response.getBody(), toscaFileStream);
-            }
-            toscaFileStream.close();
-        } catch (IOException e) {
-            LOGGER.error("cannot get TOSCA File for url " + toscaModelUrl, e);
-        }
-        return toscaFile;
+		String fileName = "temptoscafile/" + System.currentTimeMillis() + "tosca.csar";
+		ResponseEntity<byte[]> response = callSdcWithAttachment(callURI.build().encode().toUri());
+		File toscaFile = new File(fileName);
+		try {
+			FileOutputStream toscaFileStream = new FileOutputStream(toscaFile);
+			if (response != null) {
+				IOUtils.write(response.getBody(), toscaFileStream);
+			}
+			toscaFileStream.close();
+		} catch (IOException e) {
+			LOGGER.error("cannot get TOSCA File for url " + toscaModelUrl, e);
+		}
+		return toscaFile;
 
-    }
+	}
 
-    public Path getServiceToscaModel(String uuid) throws IOException {
-        StringBuilder urlBuilder = new StringBuilder().append(sdcHost).append(OnapComponentsUrlPaths.SDC_ROOT_URL)
-                .append("/").append(uuid).append(OnapComponentsUrlPaths.SDC_TOSCA_PATH);
+	public Path getServiceToscaModel(String uuid) throws IOException {
+		StringBuilder urlBuilder = new StringBuilder().append(sdcHost).append(OnapComponentsUrlPaths.SDC_ROOT_URL)
+				.append("/").append(uuid).append(OnapComponentsUrlPaths.SDC_TOSCA_PATH);
 
-        UriComponentsBuilder callURI = UriComponentsBuilder.fromHttpUrl(urlBuilder.toString());
+		UriComponentsBuilder callURI = UriComponentsBuilder.fromHttpUrl(urlBuilder.toString());
 
-        InputStream inputStream = (InputStream) callSdc(callURI.build().encode().toUri()).getBody();
+		InputStream inputStream = (InputStream) callSdc(callURI.build().encode().toUri()).getBody();
 
-        return createTmpFile(inputStream);
-    }
+		return createTmpFile(inputStream);
+	}
 
-    private Path createTmpFile(InputStream csarInputStream) throws IOException {
-        Path csarFile = Files.createTempFile("csar", ".zip");
-        Files.copy(csarInputStream, csarFile, StandardCopyOption.REPLACE_EXISTING);
+	private Path createTmpFile(InputStream csarInputStream) throws IOException {
+		Path csarFile = Files.createTempFile("csar", ".zip");
+		Files.copy(csarInputStream, csarFile, StandardCopyOption.REPLACE_EXISTING);
 
-        LOGGER.debug("Tosca file was saved at: {} ", csarFile.toAbsolutePath());
+		LOGGER.debug("Tosca file was saved at: {} ", csarFile.toAbsolutePath());
 
-        return csarFile;
-    }
+		return csarFile;
+	}
 
-    private HttpEntity<String> buildRequestHeader() {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-        httpHeaders.add(HEADER_ECOMP_INSTANCE_ID, ecompInstanceId);
-        httpHeaders.add(HEADER_AUTHORIZATION, sdcHeaderAuthorization);
-        return new HttpEntity<>("parameters", httpHeaders);
-    }
+	private HttpEntity<String> buildRequestHeader() {
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+		httpHeaders.add(HEADER_ECOMP_INSTANCE_ID, ecompInstanceId);
+		httpHeaders.add(HEADER_AUTHORIZATION, sdcHeaderAuthorization);
+		return new HttpEntity<>("parameters", httpHeaders);
+	}
 
-    private ResponseEntity<Object> callSdc(URI callURI) {
-        ResponseEntity<Object> response =
-                restTemplate.exchange(callURI, HttpMethod.GET, buildRequestHeader(), Object.class);
+	private ResponseEntity<Object> callSdc(URI callURI) {
+		ResponseEntity<Object> response = restTemplate.exchange(callURI, HttpMethod.GET, buildRequestHeader(),
+				Object.class);
 
-        if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug("response body : {} ", response.getBody().toString());
-        }
-        LOGGER.info("response status : {}", response.getStatusCodeValue());
-        loggDebugIfResponseKo(callURI.toString(), response);
-        return response;
-    }
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.debug("response body : {} ", response.getBody().toString());
+		}
+		LOGGER.info("response status : {}", response.getStatusCodeValue());
+		loggDebugIfResponseKo(callURI.toString(), response);
+		return response;
+	}
 
-    private ResponseEntity<byte[]> callSdcWithAttachment(URI callURI) {
-        try {
-            ResponseEntity<byte[]> response =
-                    restTemplate.exchange(callURI, HttpMethod.GET, buildRequestHeader(), byte[].class);
-            LOGGER.info("response status : " + response.getStatusCodeValue());
-            if (LOGGER.isWarnEnabled() && !response.getStatusCode().equals(HttpStatus.OK)) {
-                LOGGER.warn("HTTP call SDC on {} returns {} ", callURI.toString(), response.getStatusCodeValue());
-            }
-            return response;
+	private ResponseEntity<byte[]> callSdcWithAttachment(URI callURI) {
+		try {
+			ResponseEntity<byte[]> response = restTemplate.exchange(callURI, HttpMethod.GET, buildRequestHeader(),
+					byte[].class);
+			LOGGER.info("response status : " + response.getStatusCodeValue());
+			if (LOGGER.isWarnEnabled() && !response.getStatusCode().equals(HttpStatus.OK)) {
+				LOGGER.warn("HTTP call SDC on {} returns {} ", callURI.toString(), response.getStatusCodeValue());
+			}
+			return response;
 
-        } catch (BackendFunctionalException e) {
-            LOGGER.error("HTTP call SDC on {} error : {}", callURI.toString(), e);
-            return null;
-        }
-    }
+		} catch (BackendFunctionalException e) {
+			LOGGER.error("HTTP call SDC on {} error : {}", callURI.toString(), e);
+			return null;
+		}
+	}
 
-    private void loggDebugIfResponseKo(String callURI, ResponseEntity<Object> response) {
-        if (LOGGER.isWarnEnabled() && !response.getStatusCode().equals(HttpStatus.OK)) {
-            LOGGER.warn("HTTP call SDC on {} returns {} , {}", callURI, response.getStatusCodeValue(),
-                    response.getBody().toString());
-        }
-    }
+//changes for Post implementation start
+	/**
+	 * 
+	 * @param callURI
+	 * @param obj
+	 * @param userId
+	 * @return
+	 */
+	private ResponseEntity<Object> callSdcForPost(URI callURI, Object obj, String userId) {
+		try {
+			ResponseEntity<Object> response = restTemplate.exchange(callURI, HttpMethod.POST,
+					new HttpEntity<>(obj, buildRequestHeaderForPost(userId)), Object.class);
+
+			if (null == response) {
+				return null;
+			} else {
+
+				if (LOGGER.isDebugEnabled()) {
+					LOGGER.debug("response body : {} ", response.getBody().toString());
+				}
+				LOGGER.info("response status : {}", response.getStatusCodeValue());
+				return response;
+			}
+
+		} catch (BackendFunctionalException | ResourceAccessException e) {
+			LOGGER.error("Error on calling SDC API " + " ," + e.getMessage());
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	/**
+	 * 
+	 * @param serviceCatalogObject
+	 * @param userId
+	 */
+	public Map callPost(HashMap<Object, Object> serviceCatalogObject, String userId) {
+		// post url is the same as find url
+		UriComponentsBuilder callURI = UriComponentsBuilder.fromHttpUrl(sdcFindUrl);
+		ResponseEntity<Object> response = callSdcForPost(callURI.build().encode().toUri(), serviceCatalogObject,
+				userId);
+		// return (List<LinkedHashMap>) response.getBody();
+		return (LinkedHashMap) response.getBody();
+	}
+
+	/**
+	 * 
+	 * @param userId
+	 * @return
+	 */
+	private HttpHeaders buildRequestHeaderForPost(String userId) {
+		HttpHeaders httpHeaders = new HttpHeaders();
+
+		httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+		httpHeaders.add(HEADER_ECOMP_INSTANCE_ID, ecompInstanceId);
+		httpHeaders.add(HEADER_AUTHORIZATION, sdcHeaderAuthorization);
+		httpHeaders.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		httpHeaders.add(USER_ID, userId);
+
+		return httpHeaders;
+	}
+
+//changes for post implementation end
+	private void loggDebugIfResponseKo(String callURI, ResponseEntity<Object> response) {
+		if (LOGGER.isWarnEnabled() && !response.getStatusCode().equals(HttpStatus.OK)) {
+			LOGGER.warn("HTTP call SDC on {} returns {} , {}", callURI, response.getStatusCodeValue(),
+					response.getBody().toString());
+		}
+	}
 }
